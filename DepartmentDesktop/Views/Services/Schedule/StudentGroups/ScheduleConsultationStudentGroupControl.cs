00701a15c2fa08693344970b58244bb15265bb18
kotcheshir73 <kotcheshir73@gmail.com>
2017-03-11 13:07:19 +0400

    Добавлен контрол для консультаций по группе

---
+++ DepartmentDesktop/Views/Services/Schedule/StudentGroups/ScheduleConsultationStudentGroupControl.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DepartmentDesktop.Views.Services.Schedule
+{
+    public partial class ScheduleConsultationStudentGroupControl : UserControl
+    {
+        public ScheduleConsultationStudentGroupControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
