86dc629e2c123932f7e6dbdfef2a94c164e5187c
kotcheshir73 <kotcheshir73@gmail.com>
2017-04-08 13:31:38 +0400

    Распредление нагрузки. Форма

---
+++ DepartmentDesktop/Views/EducationalProcess/LoadDistribution/LoadDistributionForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace DepartmentDesktop.Views.EducationalProcess.LoadDistribution
+{
+	partial class LoadDistributionForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.comboBoxAcademicYear = new System.Windows.Forms.ComboBox();
+			this.labelAcademicYear = new System.Windows.Forms.Label();
+			this.buttonClose = new System.Windows.Forms.Button();
+			this.buttonSave = new System.Windows.Forms.Button();
+			this.tabControl = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.tabControl.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// comboBoxAcademicYear
+			// 
+			this.comboBoxAcademicYear.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBoxAcademicYear.FormattingEnabled = true;
+			this.comboBoxAcademicYear.Location = new System.Drawing.Point(104, 14);
+			this.comboBoxAcademicYear.Name = "comboBoxAcademicYear";
+			this.comboBoxAcademicYear.Size = new System.Drawing.Size(222, 21);
+			this.comboBoxAcademicYear.TabIndex = 5;
+			// 
+			// labelAcademicYear
+			// 
+			this.labelAcademicYear.AutoSize = true;
+			this.labelAcademicYear.Location = new System.Drawing.Point(16, 17);
+			this.labelAcademicYear.Name = "labelAcademicYear";
+			this.labelAcademicYear.Size = new System.Drawing.Size(79, 13);
+			this.labelAcademicYear.TabIndex = 4;
+			this.labelAcademicYear.Text = "Учебный год*:";
+			// 
+			// buttonClose
+			// 
+			this.buttonClose.Location = new System.Drawing.Point(251, 41);
+			this.buttonClose.Name = "buttonClose";
+			this.buttonClose.Size = new System.Drawing.Size(75, 23);
+			this.buttonClose.TabIndex = 16;
+			this.buttonClose.Text = "Закрыть";
+			this.buttonClose.UseVisualStyleBackColor = true;
+			// 
+			// buttonSave
+			// 
+			this.buttonSave.Location = new System.Drawing.Point(170, 41);
+			this.buttonSave.Name = "buttonSave";
+			this.buttonSave.Size = new System.Drawing.Size(75, 23);
+			this.buttonSave.TabIndex = 15;
+			this.buttonSave.Text = "Сохранить";
+			this.buttonSave.UseVisualStyleBackColor = true;
+			// 
+			// tabControl
+			// 
+			this.tabControl.Controls.Add(this.tabPage1);
+			this.tabControl.Controls.Add(this.tabPage2);
+			this.tabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tabControl.Location = new System.Drawing.Point(0, 0);
+			this.tabControl.Name = "tabControl";
+			this.tabControl.SelectedIndex = 0;
+			this.tabControl.Size = new System.Drawing.Size(1187, 461);
+			this.tabControl.TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.labelAcademicYear);
+			this.tabPage1.Controls.Add(this.buttonClose);
+			this.tabPage1.Controls.Add(this.comboBoxAcademicYear);
+			this.tabPage1.Controls.Add(this.buttonSave);
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(1179, 435);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "tabPage1";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(192, 74);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "tabPage2";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// LoadDistributionForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1187, 461);
+			this.Controls.Add(this.tabControl);
+			this.Name = "LoadDistributionForm";
+			this.Text = "Распределение нагрузки";
+			this.tabControl.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ComboBox comboBoxAcademicYear;
+		private System.Windows.Forms.Label labelAcademicYear;
+		private System.Windows.Forms.Button buttonClose;
+		private System.Windows.Forms.Button buttonSave;
+		private System.Windows.Forms.TabControl tabControl;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+	}
+}
