2b7ac16f48722e7f4d021ea56419986139ccde0e
kotcheshir73 <kotcheshir73@gmail.com>
2017-04-14 20:56:46 +0400

    Добавлен сервис по преподавателям и включен в сервис по записям распределения нагрузок

--- DepartmentDesktop/Views/EducationalProcess/LoadDistribution/LoadDistributionRecordControl.cs
+++ DepartmentDesktop/Views/EducationalProcess/LoadDistribution/LoadDistributionRecordControl.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DepartmentService.IServices;
+using DepartmentDesktop.Models;
 
 namespace DepartmentDesktop.Views.EducationalProcess.LoadDistribution
 {
@@ -19,6 +20,38 @@ namespace DepartmentDesktop.Views.EducationalProcess.LoadDistribution
 		{
 			InitializeComponent();
 			_service = service;
+
+			var resultL = _service.GetLecturers();
+
+			if(!resultL.Succeeded)
+			{
+				Program.PrintErrorMessage("При загрузке списка преподавателей возникла ошибка: ", resultL.Errors);
+				return;
+			}
+
+			List<ColumnConfig> columns = new List<ColumnConfig>
+			{
+				new ColumnConfig { Name = "Id", Title = "Id", Width = 100, Visible = false },
+				new ColumnConfig { Name = "KindOfLoadName", Title = "Запись", Width = 200, Visible = true },
+				new ColumnConfig { Name = "KindOfLoadType", Title = "Тип", Width = 100, Visible = true }
+			};
+			foreach(var lecture in resultL.Result)
+			{
+				columns.Add(new ColumnConfig { Name = "Lecture" + lecture.Id, Title = lecture.LastName, Width = 50, Visible = false });
+			}
+			dataGridViewList.Columns.Clear();
+			foreach (var column in columns)
+			{
+				dataGridViewList.Columns.Add(new DataGridViewTextBoxColumn
+				{
+					HeaderText = column.Title,
+					Name = string.Format("Column{0}", column.Name),
+					ReadOnly = true,
+					Visible = column.Visible,
+					Width = column.Width.HasValue ? column.Width.Value : 0,
+					AutoSizeMode = column.Width.HasValue ? DataGridViewAutoSizeColumnMode.None : DataGridViewAutoSizeColumnMode.Fill
+				});
+			}
 		}
 	}
 }
