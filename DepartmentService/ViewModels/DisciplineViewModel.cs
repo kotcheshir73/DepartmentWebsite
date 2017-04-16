541580fdf45dd8069391ab7427058de6d7398f5a
kotcheshir73 <kotcheshir73@gmail.com>
2017-04-16 18:25:03 +0400

    Добавили блоки дисциплин

--- DepartmentService/ViewModels/DisciplineViewModel.cs
+++ DepartmentService/ViewModels/DisciplineViewModel.cs
@@ -4,6 +4,17 @@
 	{
 		public long Id { get; set; }
 
+		public long DisciplineBlockId { get; set; }
+
 		public string DisciplineName { get; set; }
+
+		public string DisciplineBlockTitle { get; set; }
+	}
+
+	public class DisciplineBlockViewModel
+	{
+		public long Id { get; set; }
+
+		public string Title { get; set; }
 	}
 }
