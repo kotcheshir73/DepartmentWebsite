5383122ccf3552580c8e74cbd83767388fc351a4
Anton Skalkin <anton.skalkin73@gmail.com>
2018-05-11 15:29:11 +0400

    Начало создание макета

---
+++ DepartmentWpf/EditTeacherWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DepartmentWpf
+{
+    /// <summary>
+    /// Логика взаимодействия для EditTeacherWindow.xaml
+    /// </summary>
+    public partial class EditTeacherWindow : Window
+    {
+        public EditTeacherWindow()
+        {
+            InitializeComponent();
+        }
+
+        public EditTeacherWindow(Dictionary<string, string> info)
+        {
+            InitializeComponent();
+
+            textBoxFirstName.Text = "Евгений";
+            textBoxLastName.Text = "Эгов";
+            textBoxPatronymic.Text = "НИколаевич";
+        }
+
+        private void buttonOk_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ DepartmentWpf/TeachersWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DepartmentWpf
+{
+    /// <summary>
+    /// Логика взаимодействия для TeachersWindow.xaml
+    /// </summary>
+    public partial class TeachersWindow : Window
+    {
+        public TeachersWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void addMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            EditTeacherWindow editTeacherWindow = new EditTeacherWindow();
+            editTeacherWindow.ShowDialog();
+        }
+
+        private void editMenuItem_Click(object sender, RoutedEventArgs e)
+        {
+            var info = this.dataGridTeachers.SelectedItem;
+            EditTeacherWindow editTeacherWindow = new EditTeacherWindow(new Dictionary<string, string>());
+            editTeacherWindow.ShowDialog();
+        }
+    }
+}
