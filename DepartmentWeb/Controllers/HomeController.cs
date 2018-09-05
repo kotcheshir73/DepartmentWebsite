b9ff5df417089f42a6084ba7599134aa7caaef2d
StroevaJ <stroeva95mdz@gmail.com>
2018-09-05 11:07:37 +0400

    начало работы над view

---
+++ DepartmentWeb/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DepartmentModel;
+using DepartmentService;
+using DepartmentService.Context;
+using DepartmentService.Services;
+using Unity;
+using Unity.Attributes;
+
+namespace DepartmentWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        DepartmentDbContext departmentDbContext = new DepartmentDbContext();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Statement()
+        {
+            ViewBag.Message = "Your statement page.";
+            
+            return View(departmentDbContext.AcademicPlanRecords);
+        }
+    }
+}
