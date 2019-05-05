efc37d2ef2f92d4f432bcf233f5469b985fbf3bc
Anton Skalkin <anton.skalkin73@gmail.com>
2019-05-05 23:32:34 +0400

    Добавление страницы авторизации и повышение уровня минимальной винды для запуска

--- DepartmentUniversalTablet/App.xaml.cs
+++ DepartmentUniversalTablet/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DepartmentUniversalTablet.Pages;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -66,7 +67,7 @@ namespace DepartmentUniversalTablet
                     // Если стек навигации не восстанавливается для перехода к первой странице,
                     // настройка новой страницы путем передачи необходимой информации в качестве параметра
                     // параметр
-                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                    rootFrame.Navigate(typeof(AuthenticationPage), e.Arguments);
                 }
                 // Обеспечение активности текущего окна
                 Window.Current.Activate();
